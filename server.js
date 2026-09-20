0d3bc3c0189bd157543e275ee11aec46f7b620d4
b7ydrr6t9f-debug <b7ydrr6t9f@privaterelay.appleid.com>
2026-09-21 01:48:00 +0200

    Serve static files from 'public' directory
    Updated static file serving and main route to use 'public' directory.

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const app = express();
 
 const PORT = process.env.PORT || 10000;
 
-// Configura Express per servire i file statici
-app.use(express.static(path.join(__dirname)));
+// Configura Express per servire i file statici dalla cartella 'public'
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Rotta principale che restituisce la pagina web
+// Rotta principale che restituisce il file index.html dentro la cartella 'public'
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
